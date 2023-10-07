fb3a6cb90ea0d8f74c70f81ab8a4885bd0093f8a
choiseohyeon <csh200310@naver.com>
2023-10-08 01:54:00 +0900

    feat/searchResult

--- src/components/basicComponent.jsx
+++ src/components/basicComponent.jsx
@@ -1,13 +1,21 @@
-<div className="search-app">
-  <div className="center-fixed-container">
-    <div className="logo-container">
-      <img src="src/assets/main-logo.png" alt="no image" width="450"></img>
+import React from "react";
+
+function BasicComponent() {
+  return (
+    <div className="search-app">
+      <div className="center-fixed-container">
+        <div className="logo-container">
+          <img src="src/assets/main-logo.png" alt="no image" width="450" />
+        </div>
+        <div className="logo-under-container">
+          <p className="logo-under-text">
+            This page contains a list of CHI, Vis (InfoVis, SciVis, VAST), and
+            ETRA papers.
+          </p>
+        </div>
+      </div>
     </div>
-    <div className="logo-under-container">
-      <p className="logo-under-text">
-        This page contains a list of CHI, Vis (InfoVis, SciVis, VAST), and ETRA
-        papers.
-      </p>
-    </div>
-  </div>
-</div>;
+  );
+}
+
+export default BasicComponent;
---
+++ src/components/jsonData.jsx
@@ -0,0 +1,11 @@
+import chi00 from "../data/chi00.json";
+import chi01 from "../data/chi01.json";
+import chi02 from "../data/chi02.json";
+
+const jsonData = {
+  chi00,
+  chi01,
+  chi02,
+};
+
+export default jsonData;
