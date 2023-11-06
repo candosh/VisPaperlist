3ee80126a85ddf5e93153033428fe5082333c6e5
choiseohyeon <csh200310@naver.com>
2023-11-06 13:17:05 +0900

    feat/backend-server

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:3000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
